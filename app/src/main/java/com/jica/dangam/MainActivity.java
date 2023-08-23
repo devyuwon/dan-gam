a34399718b3d47a1eec56400e25690172e06eed3
{이승한} <{dev.seunghan.lee@gmail.com}>
2023-08-23 15:51:06 +0900

    #5 툴바, 토글버튼, 위치표시 레이아웃 프레임 제작

--- app/src/main/java/com/jica/dangam/MainActivity.java
+++ app/src/main/java/com/jica/dangam/MainActivity.java
@@ -1,7 +1,10 @@
 package com.jica.dangam;
 
+import android.view.Menu;
+
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		Toolbar tb = (Toolbar) findViewById(R.id.mainToolbar) ;
+		setSupportActionBar(tb) ;
+
+
+
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main_toolbar, menu) ;
+		return super.onCreateOptionsMenu(menu);
 	}
 }
