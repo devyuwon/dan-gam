e012ab892dbe54e4fc9bcc043e9d8f0316b187e2
yeddorii <dev.yeseul2@gmail.com>
2023-08-23 18:21:15 +0900

    #11 feat : 공고 수정/삭제/등록 디자인 목업

---
+++ app/src/main/java/com/jica/dangam/advertising_ilgam.java
@@ -0,0 +1,13 @@
+package com.jica.dangam;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class advertising_ilgam extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_advertising_ilgam);
+	}
+}
---
+++ app/src/main/java/com/jica/dangam/advertising_modify_toast.java
@@ -0,0 +1,13 @@
+package com.jica.dangam;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class advertising_modify_toast extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_advertising_modify_toast);
+	}
+}
