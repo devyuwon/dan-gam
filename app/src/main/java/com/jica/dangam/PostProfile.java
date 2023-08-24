4e059dd1bcdbda5470f7cde67fd7d339213d5f29
{이승한} <{dev.seunghan.lee@gmail.com}>
2023-08-24 16:27:10 +0900

    #14 공고 객체용 클래스 작성 및 데이터베이스 연동 테스트 기초 설정

---
+++ app/src/main/java/com/jica/dangam/PostProfile.java
@@ -0,0 +1,65 @@
+package com.jica.dangam;
+
+public class PostProfile {
+
+	private int postNumber;
+	private String title;
+	private String contents;
+	private String location;
+
+	//구현 예정
+	//private String image;
+
+	public PostProfile(){
+		this.postNumber=0;
+		this.title="No Title";
+		this.contents="No Contents";
+		this.location="missing";
+	}
+
+	public PostProfile(int postNumber, String title, String contents, String location){
+		this.postNumber=postNumber;
+		this.title=title;
+		this.contents=contents;
+		this.location=location;
+		//this.image=image;
+	}
+	//getter
+	public int getPostNumber(){
+		return postNumber;
+	}
+	public String getTitle(){
+		return title;
+	}
+	public String getContents(){
+		return contents;
+	}
+	public String getLocation(){
+		return location;
+	}
+
+	//구현 예정
+	//public String getImage(){
+	//	return image;
+	//}
+
+	//setter
+
+	public void setPostNumber(int postNumber){
+		this.postNumber=postNumber;
+	}
+	public void setTitle(String title){
+		this.title=title;
+	}
+	public void  setContents(String contents){
+		this.contents=contents;
+	}
+	public void setLocation(String location){
+		this.location=location;
+	}
+
+	//구현 예정
+	//public void setImage(String image){
+	//	this.image=image;
+	//}
+}
