9c38756c93834fb89acfdf4e2cee2cb4aa2108d7
devyuwon <yuwon9207@gmail.com>
2023-08-29 15:59:54 +0900

    #4 Feat: Google Login 구현

--- app/src/main/java/com/jica/dangam/LoginActivity.java
+++ app/src/main/java/com/jica/dangam/LoginActivity.java
@@ -1,13 +1,131 @@
 package com.jica.dangam;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+import android.content.Intent;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
+	private SignInButton btnGoogleLogin;
+	private Button btnGoogleLogout;
+	private GoogleSignInClient mGoogleSignInClient;
+	private FirebaseAuth mAuth;
+	private int RC_SIGN_IN = 9001;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+
+		btnGoogleLogin = findViewById(R.id.btnGoogleLogin);
+
+		GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+			.requestIdToken(getString(R.string.default_web_client_id))
+			.requestEmail()
+			.build();
+
+		mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+		mAuth = FirebaseAuth.getInstance();
+
+		btnGoogleLogin.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				signIn();
+			}
+		});
+
+		btnGoogleLogout = findViewById(R.id.btnGoogleLogout);
+		btnGoogleLogout.setOnClickListener(view -> {
+			signOut();
+		});
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		FirebaseUser currentUser = mAuth.getCurrentUser();
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+
+		if (requestCode == RC_SIGN_IN) {
+			Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+
+			try {
+				GoogleSignInAccount account = task.getResult(ApiException.class);
+				firebaseAuthWithGoogle(account);
+			} catch (ApiException e) {
+				Log.w("LoginActivity", "Google Sign in failed", e);
+			}
+		}
+	}
+
+	private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+		Log.d("LoginActivity", "firebaseAuthWithGoogle:" + acct.getId());
+
+		AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+		mAuth.signInWithCredential(credential)
+			.addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+				@Override
+				public void onComplete(@NonNull Task<AuthResult> task) {
+					if (task.isSuccessful()) {
+						Log.d("LoginActivity", "signInWithCredential: Success");
+						FirebaseUser user = mAuth.getCurrentUser();
+
+						Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+						startActivity(intent);
+						finish();
+					} else {
+						Log.w("LoginActivity", "sighInWithCredential: Failure", task.getException());
+					}
+				}
+			});
+	}
+
+	private void signIn() {
+		Intent sighInIntent = mGoogleSignInClient.getSignInIntent();
+		startActivityForResult(sighInIntent, RC_SIGN_IN);
+	}
+
+	private void signOut() {
+		mAuth.signOut();
+
+		mGoogleSignInClient.signOut().addOnCompleteListener(this, new OnCompleteListener<Void>() {
+			@Override
+			public void onComplete(@NonNull Task<Void> task) {
+				Toast.makeText(getApplicationContext(), "Logout", Toast.LENGTH_SHORT).show();
+			}
+		});
+	}
+
+	private void revokeAccess() {
+		mAuth.signOut();
+
+		mGoogleSignInClient.revokeAccess().addOnCompleteListener(this, new OnCompleteListener<Void>() {
+			@Override
+			public void onComplete(@NonNull Task<Void> task) {
+				Toast.makeText(getApplicationContext(), "Logout", Toast.LENGTH_SHORT).show();
+			}
+		});
 	}
 }
--- app/src/main/java/com/jica/dangam/MainActivity.java
+++ app/src/main/java/com/jica/dangam/MainActivity.java
@@ -1,23 +1,13 @@
 package com.jica.dangam;
 
-import com.google.firebase.auth.FirebaseAuth;
-
-import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-	private FirebaseAuth oAuth;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
-		if (FirebaseAuth.getInstance().getCurrentUser() == null) {
-			Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-			startActivity(intent);
-			finish();
-		}
 	}
 }
