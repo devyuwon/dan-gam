8167255de293cc711cc729879d8879ab63f5e877
yeddorii <dev.yeseul2@gmail.com>
2023-09-04 17:14:23 +0900

    #11 Feat: 갤러리에서 (단일)사진 첨부 기능 추가로 다중사진 첨부로 변경 예정

--- app/src/main/java/com/jica/dangam/PostActivity.java
+++ app/src/main/java/com/jica/dangam/PostActivity.java
@@ -1,15 +1,20 @@
 package com.jica.dangam;
 
+import java.util.ArrayList;
+
 import android.content.Intent;
+import android.net.Uri;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class PostActivity extends AppCompatActivity {
 
@@ -17,18 +22,21 @@ public class PostActivity extends AppCompatActivity {
 	ImageButton btn_post_delete;
 	Button btn_post_menu;
 	ConstraintLayout delete_popup;
-	Button btn_delete_yes,btn_delete_no;
+	Button btn_delete_yes, btn_delete_no;
 	RadioGroup rg_post_state_modify;
 	Button rg_post_close;
 	FrameLayout lo_post_main;
+	TextView tv_post_title;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_post);
 
+		String test = getIntent().getStringExtra("푸바오");
 
 		btn_post_modify = findViewById(R.id.btn_post_modify);
+		//btn_post_modify.setText(test);
 		btn_post_delete = findViewById(R.id.btn_post_delete);
 
 		btn_post_menu = findViewById(R.id.btn_post_menu);
@@ -40,6 +48,8 @@ public class PostActivity extends AppCompatActivity {
 		btn_delete_no = findViewById(R.id.btn_delete_no);
 
 		lo_post_main = findViewById(R.id.lo_post_main);
+
+		tv_post_title = findViewById(R.id.tv_post_title);
 
 		//메뉴버튼
 		btn_post_menu.setOnClickListener(new View.OnClickListener() {
@@ -55,7 +65,6 @@ public class PostActivity extends AppCompatActivity {
 				rg_post_state_modify.setVisibility(view.INVISIBLE);
 			}
 		});
-
 
 		//수정 버튼
 		btn_post_modify.setOnClickListener(new View.OnClickListener() {
@@ -87,15 +96,10 @@ public class PostActivity extends AppCompatActivity {
 					}
 
 				});
-			//	lo_post_main.setVisibility(view.VISIBLE);
+				//	lo_post_main.setVisibility(view.VISIBLE);
 
 			}
 		});
-
-
-
-
-
 
 	}
 }
--- app/src/main/java/com/jica/dangam/PostWriteActivity.java
+++ app/src/main/java/com/jica/dangam/PostWriteActivity.java
@@ -1,12 +1,31 @@
 package com.jica.dangam;
 
+// import com.bumptech.glide.Glide;
+// import com.bumptech.glide.annotation.GlideModule;
+// import com.bumptech.glide.request.target.CustomTarget;
+// import com.bumptech.glide.request.transition.Transition;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RadioGroup;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.content.res.AppCompatResources;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class PostWriteActivity extends AppCompatActivity {
 	String state;
@@ -14,6 +33,12 @@ public class PostWriteActivity extends AppCompatActivity {
 	Button btn_plus_gps;
 	RadioGroup rg_post_state_modify;
 	Button btn_post_complete;
+	ImageView iv_post_picture;
+	Button btn_post_picture;
+	Uri uri;
+
+	ArrayList<Uri> uriList = new ArrayList<>();
+	RecyclerView rv_post_image; // 이미지를 보여줄 리사이클러뷰
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +52,11 @@ public class PostWriteActivity extends AppCompatActivity {
 
 		btn_post_complete = findViewById(R.id.btn_post_complete);
 
+		iv_post_picture = findViewById(R.id.iv_post_picture);
+		btn_post_picture = findViewById(R.id.btn_post_picture);
+
+		rv_post_image = findViewById(R.id.rv_post_image);
+
 		// 유형 선택시 색상 변경
 		btn_ilgam.setOnClickListener(new View.OnClickListener() {
 			@Override
@@ -38,6 +68,7 @@ public class PostWriteActivity extends AppCompatActivity {
 					AppCompatResources.getColorStateList(getApplicationContext(), R.color.grey));
 			}
 		});
+
 		btn_ilgun.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
@@ -47,6 +78,59 @@ public class PostWriteActivity extends AppCompatActivity {
 					AppCompatResources.getColorStateList(getApplicationContext(), R.color.grey));
 			}
 		});
+
+		//앨범으로 이동하는 버튼
+		btn_post_picture.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				Intent intent = new Intent(Intent.ACTION_PICK);
+				intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+				intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+				intent.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+				startActivityForResult(intent, 2222);
+			}
+		});
+
+/*
+		//갤러리에서 이미지 불러오기 - 객체선언
+		ActivityResultLauncher<Intent> startActivityResult = registerForActivityResult(
+			new ActivityResultContracts.StartActivityForResult(),
+			new ActivityResultCallback<ActivityResult>() {
+				@Override
+				public void onActivityResult(ActivityResult result) {
+					if (result.getResultCode() == RESULT_OK && result.getData() != null) {
+
+						uri = result.getData().getData();
+
+						try {
+							Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+							iv_post_picture.setImageBitmap(bitmap);
+
+						} catch (FileNotFoundException e) {
+							e.printStackTrace();
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+					}
+
+				}
+			}
+		);
+
+		//사진불러오기
+		btn_post_picture.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				Intent intent = new Intent(Intent.ACTION_PICK);
+				intent.setType("image/*");
+				startActivityResult.launch(intent);
+
+			}
+		});
+
+
+
+*/
 
 
 	/*	//모집 상태
@@ -73,6 +157,7 @@ public class PostWriteActivity extends AppCompatActivity {
 			@Override
 			public void onClick(View view) {
 				Intent intent = new Intent(getApplicationContext(), PostActivity.class);
+				intent.putExtra("푸바오", "안녕하세요");
 				startActivity(intent);
 			}
 		});
