bdda5d1855dd1c9488a364794f016893e853a1d1
devyuwon <dev.yuwon@gmail.com>
2023-09-15 22:35:24 +0900

    #32 Feat: BottomSheet 내 Map 구현

--- app/src/main/java/com/jica/dangam/map/MapBottomSheetDialog.java
+++ app/src/main/java/com/jica/dangam/map/MapBottomSheetDialog.java
@@ -1,18 +1,41 @@
 package com.jica.dangam.map;
 
+import java.util.Arrays;
+
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 import com.jica.dangam.R;
 
+import android.Manifest;
 import android.app.Dialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentManager;
 
-public class MapBottomSheetDialog extends BottomSheetDialogFragment {
+import com.naver.maps.map.LocationTrackingMode;
+import com.naver.maps.map.MapFragment;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.util.FusedLocationSource;
+
+public class MapBottomSheetDialog extends BottomSheetDialogFragment implements OnMapReadyCallback {
+	private FusedLocationSource locationSource;
+	private NaverMap mNaverMap;
+	private static final int PERMISSION_REQUEST_CODE = 100;
+	private static final String[] PERMISSION = {
+		Manifest.permission.ACCESS_FINE_LOCATION,
+		Manifest.permission.ACCESS_COARSE_LOCATION
+	};
+
 	@Override
 	public void onStart() {
 		super.onStart();
@@ -40,7 +63,36 @@ public class MapBottomSheetDialog extends BottomSheetDialogFragment {
 
 		view.setMinimumHeight(pageHeight - px);
 
+		FragmentManager fragmentManager = getChildFragmentManager();
+		MapFragment mapFragment = (MapFragment) fragmentManager.findFragmentById(R.id.map);
+		if (mapFragment == null) {
+			mapFragment = MapFragment.newInstance();
+			fragmentManager.beginTransaction().add(R.id.map, mapFragment).commit();
+		}
+
+		mapFragment.getMapAsync(this);
+		locationSource = new FusedLocationSource(this, PERMISSION_REQUEST_CODE);
+
 		return view;
+	}
+
+	@Override
+	public void onMapReady(@NonNull NaverMap naverMap) {
+		mNaverMap = naverMap;
+		mNaverMap.setLocationSource(locationSource);
+		ActivityCompat.requestPermissions(getActivity(), PERMISSION, PERMISSION_REQUEST_CODE);
+	}
+
+	@Override
+	public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+		@NonNull int[] grantResults) {
+		super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+		if (requestCode == PERMISSION_REQUEST_CODE) {
+			if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+				mNaverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
+			}
+		}
 	}
 
 	public int dpToPx (int dp) {
