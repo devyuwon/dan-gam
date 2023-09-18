ed896ec08235213298161d7c2e7f6402afe4ce93
devyuwon <yuwon9207@gmail.com>
2023-09-18 13:05:10 +0900

    #32 Feat: Map GPS 위치 Button 추가

--- app/src/main/java/com/jica/dangam/map/MapBottomSheetDialog.java
+++ app/src/main/java/com/jica/dangam/map/MapBottomSheetDialog.java
@@ -1,18 +1,13 @@
 package com.jica.dangam.map;
 
-import java.util.Arrays;
-
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 import com.jica.dangam.R;
 
 import android.Manifest;
 import android.app.Dialog;
-import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,16 +20,18 @@ import com.naver.maps.map.LocationTrackingMode;
 import com.naver.maps.map.MapFragment;
 import com.naver.maps.map.NaverMap;
 import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.UiSettings;
+import com.naver.maps.map.overlay.LocationOverlay;
 import com.naver.maps.map.util.FusedLocationSource;
 
 public class MapBottomSheetDialog extends BottomSheetDialogFragment implements OnMapReadyCallback {
-	private FusedLocationSource locationSource;
-	private NaverMap mNaverMap;
 	private static final int PERMISSION_REQUEST_CODE = 100;
 	private static final String[] PERMISSION = {
 		Manifest.permission.ACCESS_FINE_LOCATION,
 		Manifest.permission.ACCESS_COARSE_LOCATION
 	};
+	private FusedLocationSource locationSource;
+	private NaverMap naverMap;
 
 	@Override
 	public void onStart() {
@@ -64,7 +61,7 @@ public class MapBottomSheetDialog extends BottomSheetDialogFragment implements O
 		view.setMinimumHeight(pageHeight - px);
 
 		FragmentManager fragmentManager = getChildFragmentManager();
-		MapFragment mapFragment = (MapFragment) fragmentManager.findFragmentById(R.id.map);
+		MapFragment mapFragment = (MapFragment)fragmentManager.findFragmentById(R.id.map);
 		if (mapFragment == null) {
 			mapFragment = MapFragment.newInstance();
 			fragmentManager.beginTransaction().add(R.id.map, mapFragment).commit();
@@ -77,25 +74,31 @@ public class MapBottomSheetDialog extends BottomSheetDialogFragment implements O
 	}
 
 	@Override
-	public void onMapReady(@NonNull NaverMap naverMap) {
-		mNaverMap = naverMap;
-		mNaverMap.setLocationSource(locationSource);
-		ActivityCompat.requestPermissions(getActivity(), PERMISSION, PERMISSION_REQUEST_CODE);
+	public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+		@NonNull int[] grantResults) {
+
+		if (!locationSource.isActivated()) {
+			naverMap.setLocationTrackingMode(LocationTrackingMode.None);
+		}
+		super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 	}
 
 	@Override
-	public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
-		@NonNull int[] grantResults) {
-		super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+	public void onMapReady(@NonNull NaverMap naverMap) {
+		this.naverMap = naverMap;
+		naverMap.setLocationSource(locationSource);
+		naverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
 
-		if (requestCode == PERMISSION_REQUEST_CODE) {
-			if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-				mNaverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
-			}
-		}
+		UiSettings uiSettings = this.naverMap.getUiSettings();
+		uiSettings.setLocationButtonEnabled(true);
+
+		LocationOverlay locationOverlay = this.naverMap.getLocationOverlay();
+		locationOverlay.setVisible(true);
+
+		ActivityCompat.requestPermissions(getActivity(), PERMISSION, PERMISSION_REQUEST_CODE);
 	}
 
-	public int dpToPx (int dp) {
-		return (int) (dp * getResources().getDisplayMetrics().density);
+	public int dpToPx(int dp) {
+		return (int)(dp * getResources().getDisplayMetrics().density);
 	}
 }
